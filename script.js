79167a242b7e4924c92beac0e83345e053830108
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-03-15 20:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+let str2;
 let n=str.length;
+for(let k=0;k<n;k++){
+	if(str[i]!=" "){
+		str2.push(str[i]);
+	}
+}
+let str1=str2.toLowerCase();
 let i=0;
 let j=n-1;
 while(i<n/2){
-	if(str[i]!=str[j]){
+	
+	if(str1[i]!=str1[j]){
 		return false;
 	}
 	i++;
